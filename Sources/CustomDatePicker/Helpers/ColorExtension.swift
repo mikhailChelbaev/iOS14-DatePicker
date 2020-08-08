f8b84c9cef9a3a9b83b4c10a1617f372d78ffc0a
Mikhail <chelbaev.mihail70@gmail.com>
2020-08-08 22:41:52 +0300

    color test

--- Sources/CustomDatePicker/Helpers/ColorExtension.swift
+++ Sources/CustomDatePicker/Helpers/ColorExtension.swift
@@ -9,6 +9,6 @@ import UIKit
 
 extension UIColor {
     static var customBackground: UIColor = .clear
-    static var customGray3: UIColor = UIColor(named: "gray3", in: Bundle(for: CalendarView.self), compatibleWith: nil)!
-    static var customLabel: UIColor = UIColor(named: "label", in: Bundle(for: CalendarView.self), compatibleWith: nil)!
+    static var customGray3: UIColor = UIColor(named: "gray3", in: Bundle(for: CalendarView.self), compatibleWith: nil) ?? .gray
+    static var customLabel: UIColor = UIColor(named: "label", in: Bundle(for: CalendarView.self), compatibleWith: nil) ?? .black
 }
